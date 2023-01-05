fc3691cbb6eb3c394b451c4d15a3cc01082bab24
Sebastian Bergman <basse@grafikfel.org>
2023-01-05 01:38:43 +0100

    chore: update configs and schema validation

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -8,15 +8,11 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    // Enables hot-reload and easy integration for local packages
-    transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
-  },
-  // We already do linting on GH actions
-  eslint: {
-    ignoreDuringBuilds: !!process.env.CI,
-  },
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@acme/api", "@acme/auth", "@acme/db"],
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: !!process.env.CI },
+  typescript: { ignoreBuildErrors: !!process.env.CI },
 };
 
 export default config;
--- apps/nextjs/src/types.ts
+++ apps/nextjs/src/types.ts
@@ -1,5 +1,6 @@
 import { type NextPage } from "next";
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 export type NextPageWithAuth<P = {}, IP = P> = NextPage<P, IP> & {
   auth?: boolean;
 };
