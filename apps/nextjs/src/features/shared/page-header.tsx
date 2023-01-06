c12a922dbdd4f6df752da039febb4ccd01d57304
Sebastian Bergman <basse@grafikfel.org>
2023-01-06 21:49:29 +0100

    fix: only vertical padding on the header

--- apps/nextjs/src/features/shared/page-header.tsx
+++ apps/nextjs/src/features/shared/page-header.tsx
@@ -6,7 +6,7 @@ type PageHeaderProps = {
 
 export function PageHeader({ children }: PageHeaderProps) {
   return (
-    <div className="container mx-auto flex w-full flex-row items-center  p-2 align-middle">
+    <div className="container mx-auto flex w-full flex-row items-center py-2 align-middle">
       <div className="mr-2 text-2xl">Conquest</div>
       {children}
     </div>
